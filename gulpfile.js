fa17fabeeed665bf0711685e51888a18f352f3c6
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-12-08 22:26:50 +0300

    fix: setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('scripts', function () {
+  return gulp.src('./src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./src/css/*.css')
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('html', function() {
+  return gulp.src('./src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', gulp.series('scripts', 'styles', 'html'))
